6df70d1abdfb17dcc7f28a0895392c428a02fbeb
USAMI Kenta <tadsan@zonu.me>
2017-05-05 17:23:42 +0900

    Add test for Mastodon\session function

---
+++ tests/functions/SessionTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Baguette\Mastodon\functions;
+
+use Baguette\Mastodon;
+use Baguette\Mastodon\Service\SessionStorage;
+
+/**
+ * session function
+ *
+ * @author    USAMI Kenta <tadsan@zonu.me>
+ * @copyright 2017 Baguette HQ
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0
+ */
+class SessionTest extends \Baguette\Mastodon\TestCase
+{
+    public function test()
+    {
+        $actual = \Baguette\Mastodon\session(
+            'mstdn.example.social',
+            'xxxxxxxx', 'yyyyyyyy',
+            [
+                'scope' => 'read',
+                'credential' => [
+                    'username' => 'sample',
+                    'password' => 'xyzxyz',
+                ],
+                'authorization' => [
+                    'access_token' => 'zzzzzzzzz',
+                    'token_type'   => 'bearer',
+                    'scope'        => 'read',
+                    'created_at'   => 123456789,
+                ],
+            ]
+        );
+
+        $this->assertInstanceOf(Mastodon\Mastodon::class, $actual);
+        $this->assertInstanceOf(Mastodon\Client::class, $actual->client);
+        $this->assertInstanceOf(SessionStorage::class, $actual->session);
+    }
+}
