b5d60422f7ee0bf4147038f6d043ac0c3fc058ba
USAMI Kenta <tadsan@zonu.me>
2017-05-27 16:46:08 +0900

    Current Mastodon API does not accept multiple redirect_uri

--- sample/inc/app.php
+++ sample/inc/app.php
@@ -23,17 +23,24 @@ namespace app
         $file = strtr($client->getHostname(), ['/' => '_']);
         $path = __DIR__ . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, ['..', 'cache', "{$file}.json"]);
 
+        $callback_uri = array_shift($callback_uris);
+        if (count($callback_uris) > 0) {
+            trigger_error('Current Mastodon API does not accept multiple redirect_uri.', E_USER_NOTICE);
+        }
+
         if (is_file($path)) {
             $app = file_get_contents($path);
         } else {
             $create_app_path = sprintf('%s://%s/api/v1/apps', $client->getScheme(), $client->getHostname());
-            $response = m\http()->request('POST', $create_app_path, [
+            $request_options = [
                 'form_params' => [
                     'client_name' => \SERVICE_NAME . " ip",
                     'scope' => (string)$scope,
-                    'redirect_uris' => $callback_uris,
-                ],
-            ]);
+                    'redirect_uris' => $callback_uri,
+                ]
+            ];
+
+            $response = m\http()->request('POST', $create_app_path, $request_options);
             $app = (string)$response->getBody();
             file_put_contents($path, $app);
         }
